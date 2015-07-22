b511a2fb54fb1843d2c484c61d531cfbc34a60a7
Aaron Kusmec <amkusmec@iastate.edu>
2015-07-22 11:47:43 -0500

    Completed find_alleles

--- src/find_alleles.py
+++ src/find_alleles.py
@@ -85,16 +85,98 @@ def checkFile(filename, modei):
     elif modei == 2:
         if line[0] != "rs" and line[0] != "rs#":
             warning(".hmp.txt formatted incorrectly.")
-    elif modei == 3:
-        if len(line) <= 6:
-            warning(".ped missing genotypes.")
-            
-        # Check the map file as well
-        with open(filename.split('.')[0] + ".map", 'r') as infile:
-            line = infile.readline().split()
-        if len(line) != 4:
-            warning(".map incorrectly formatted.")
     else:
         warning("Unrecognized file format.")
     
     print("[ ", filename, " ] is appropriately formatted.")
+
+###############################################################################
+def findAllelesDsf(inname, outname):
+    with open(inname, 'r') as infile:
+        with open(outname, 'w') as outfile:
+            header = infile.readline()
+            outfile.write(header)
+
+            for line in infile:
+                line = line.split('\t')
+                major, minor = majorMinor(line[5:])
+                line[1], line[2] = major, minor
+                outfile.write('\t'.join(line) + '\n')
+
+###############################################################################
+def findAllelesHmp(inname, outname):
+    with open(inname, 'r') as infile:
+        with open(outname, 'w') as outfile:
+            header = infile.readline()
+            outfile.write(header)
+
+            for line in infile:
+                line = line.split('\t')
+                major, minor = majorMinor(line[11:])
+                line[1] = major + '/' + minor
+                outfile.write('\t'.join(line) + '\n')
+
+###############################################################################
+def majorMinor(snp):
+    set0 = set(snps)
+    alleles = list(set0)
+
+    for k in ['W', 'S', 'M', 'K', 'R', 'Y', '0']:
+        if k in alleles:
+            alleles += iupac2[k].split()
+
+    set0 = set(alleles)
+    set0 = set0.difference(set(['W', 'S', 'M', 'K', 'R', 'Y', '0', 'N']))
+    alleles = list(set0)
+
+    if len(alleles) == 2:
+        het = iupac[''.join(alleles)]
+        allele1 = 2*snps.count(alleles[0]) + snps.count(het)
+        allele2 = 2*snps.count(alleles[1]) + snps.count(het)
+
+        if allele1 >= allele2:
+            return alleles[0], alleles[1]
+        else:
+            return alleles[1], alleles[0]
+
+###############################################################################
+if __name__ == '__main__':
+    parser = get_parser()
+    args = vars(parser.parse_args())
+    
+    # Change the working directory if necessary
+    if args['path'] is not None:
+        os.chdir(args['path'])
+    if args['input'] is None:
+        warning("No input file.")
+    if args['output'] is None:
+        warning("No output file.")
+    
+    print(version())
+    
+    st = timeit.default_timer()
+    
+    # Load file
+    checkFile(args['input'], args['modei'])
+    snps = readFile(args['input'], args['modei'])
+    
+    # Convert
+    if args['modei'] == 1:
+        findAllelesDsf(args['input'], args['output'])
+    elif args['modei'] == 2:
+        findAllelesHmp(args['input'], args['output'])
+    else:
+        warning("Unrecognized input/output mode(s).")
+    
+    et = timeit.default_timer()
+
+    print("Conversion finished.")
+    print("Time: %.2f min." % ((et - st)/60))
+
+
+
+
+
+
+
+
